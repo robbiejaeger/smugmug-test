b3ef1d4f8b09e88ec711e07cbd6272c3853f01b8
Robbie Jaeger <jaeger.rob@gmail.com>
2017-10-11 14:57:59 -0600

    Add request library and basic smugmug request

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
+const rp = require('request-promise-native');
+require('dotenv').config();
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
-  response.render('home');
+  const options = {
+    method: 'GET',
+    uri: 'https://www.smugmug.com/api/v2/user/robbiejaeger?APIKey=' + process.env.SMUGMUG_API_KEY,
+    headers: {
+        'Accept': 'application/json'
+    },
+    json: true
+  };
+
+  rp(options)
+    .then((res) => {
+      console.log(res)
+      response.render('home', { smugResponse: JSON.stringify(res, null, 2) });
+    })
+    .catch(
+      (err) => {console.error("error", { err });
+    })
 });
 
 app.listen(app.get('port'), () => {
